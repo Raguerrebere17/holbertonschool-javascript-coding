fac0575902d547d1aaa7736909a60697e8f1815e
Raguerrebere17 <raguerrebere17@gmail.com>
2023-09-24 05:30:06 -0700

    Missing letter ('s') added

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -8,7 +8,7 @@ request.get(process.argv[2], (err, response) => {
   }
   let count = 0;
   const movies = JSON.parse(response.body);
-  movies.result.forEach(movie => {
+  movies.results.forEach(movie => {
     movie.characters.forEach(character => {
       if (character.includes('/people/18')) count++;
     });
